d6d395b010673f2ca9ccf8f5df96f8e5fea497c0
Myron Smith <myronsmithofficial@gmail.com>
2026-09-15 16:04:24 -0400

    Enable live site presence tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import SitePresence from "@/components/SitePresence";
 import "./globals.css";
 
 export const viewport = {
@@ -33,6 +34,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <SitePresence />
         {children}
       </body>
     </html>
